a35a46520a1fb01e4502f794e502c7befdb4635f
Razieh <raziehamouzgar@gmail.com>
2022-10-24 11:20:13 +0330

    addrd UserList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
   return (
     <div className="App">
      <AddUser />
+     <UserList users={[]}/>
     </div>
   );
 }
---
+++ src/components/Users/UserList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './UserList.moduls.css'
+
+const UserList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
